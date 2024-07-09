5fddfb7cef757603c5645a1c04184fb6e59ce73e
WuMing <3261937664@qq.com>
2024-07-09 16:50:42 +0800

    add fonts

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import App from 'App'
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
+import 'index.css'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
