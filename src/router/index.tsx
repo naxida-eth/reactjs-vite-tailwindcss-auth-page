3591082a1f7c3f70b1b43a455396c56bb10b265f
WuMing <3261937664@qq.com>
2024-07-10 11:24:41 +0800

    fix:routegurad

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
+import React from 'react'
+import { createBrowserRouter, RouteObject } from 'react-router-dom'
 import Layout from 'layout'
 import Home from 'pages/Home'
-import { createBrowserRouter, RouteObject } from 'react-router-dom'
 import Login from 'pages/Login'
 import GuardRoute from './GuardRoute'
 
@@ -31,13 +32,28 @@ const routes: RouteConfig[] = [
   }
 ]
 
-export default createBrowserRouter(
-  routes.map((route) => ({
-    ...route,
-    element: <GuardRoute route={route}>{route.element}</GuardRoute>
-  }))
-)
-
 function NotFound() {
   return <h1>Not Found</h1>
 }
+
+const wrapWithGuard = (route: RouteConfig): RouteConfig => {
+  if (route.auth) {
+    return {
+      ...route,
+      element: <GuardRoute route={route}>{route.element}</GuardRoute>
+    }
+  }
+
+  if (route.children) {
+    return {
+      ...route,
+      children: route.children.map(wrapWithGuard)
+    }
+  }
+
+  return route
+}
+
+const guardedRoutes = routes.map(wrapWithGuard)
+
+export default createBrowserRouter(guardedRoutes)
